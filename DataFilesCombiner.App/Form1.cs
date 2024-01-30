e2e3207a8c0f530b9b290ce1bfdd12cec7f1e1b5
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-30 22:28:57 +0530

    wired up report generation

--- DataFilesCombiner.App/Form1.cs
+++ DataFilesCombiner.App/Form1.cs
@@ -1,6 +1,8 @@
 using DataFileCombiner.ClassLibrary.Interfaces;
+using DataFileCombiner.ClassLibrary.Mediatr.Commands;
 using DataFileCombiner.ClassLibrary.Utility;
 using DataFilesCombiner.ClassLibrary.ExtensionMethods;
+using MediatR;
 using Microsoft.Extensions.Configuration;
 using System.Diagnostics;
 
@@ -10,16 +12,20 @@ public partial class Form1 : Form
 {
 	private readonly IConfiguration _configuration;
 	private readonly IMatchingDataService _dataService;
+	private readonly IMediator _mediator;
+
 	public string matchesInfo => $"{_dataService.CheckedMatchesCount} matched records found";
 
 	public Form1(
 		IConfiguration configuration,
-		IMatchingDataService dataService
+		IMatchingDataService dataService,
+		IMediator mediator
 		)
 	{
 		InitializeComponent();
 		_configuration = configuration;
 		_dataService = dataService;
+		_mediator = mediator;
 		_dataService.NewMatchesFound += OnNewMatchesFound;
 		Matches_Lable.Text = matchesInfo;
 		Generate_Rpt_Btn.Enabled = false;
@@ -35,8 +41,17 @@ public partial class Form1 : Form
 		Process.Start("explorer.exe", _configuration.GetOutputDirectoryExistingPath());
 	}
 
-	private void Generate_Rpt_Btn_Click(object sender, EventArgs e)
+	private async void Generate_Rpt_Btn_Click(object sender, EventArgs e)
 	{
+		var reportResult = await _mediator.Send(new GenerateReportCommand());
+		if (reportResult.IsFailure)
+		{
+			MessageBox.Show("Something wrong happend. Better check logs.");
+		}
+		else
+		{
+			Output_btn_Click(sender, new EventArgs());
+		}
 	}
 	private void OnNewMatchesFound(object sender, EventArgs e)
 	{
