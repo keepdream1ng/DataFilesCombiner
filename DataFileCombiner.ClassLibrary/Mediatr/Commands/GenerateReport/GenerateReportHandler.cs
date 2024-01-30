f7498d7cf78be795c53bd1e6e3eb3d933d4edd85
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-30 23:06:02 +0530

    refactored to parrallel processing

--- DataFileCombiner.ClassLibrary/Mediatr/Commands/GenerateReport/GenerateReportHandler.cs
+++ DataFileCombiner.ClassLibrary/Mediatr/Commands/GenerateReport/GenerateReportHandler.cs
@@ -19,37 +19,13 @@ public class GenerateReportHandler(
 		if (mathedFilePaths.CsvFilePaths.Length < 1
 			|| mathedFilePaths.XmlFilePaths.Length < 1) return Result<FilePath>.Failure();
 
-		List<User> users = new();
-		foreach (FilePath path in mathedFilePaths.CsvFilePaths)
-		{
-			var result = await mediator.Send(new DeserializeUsersFromCsvQuery(path), cancellationToken);
-			if (result.IsSuccess) users.AddRange(result.Value!);
-		}
-
-
-		List<Card> cards = new();
-		foreach (FilePath path in mathedFilePaths.XmlFilePaths)
-		{
-			var result = await mediator.Send(new DeserializeCardsFromXmlQuery(path), cancellationToken);
-			if (result.IsSuccess) cards.AddRange(result.Value!.CardList);
-		}
-
-		IEnumerable<Record> combinedRecords = from card in cards
-											  join user in users on card.UserId equals user.UserId
-											  select new Record()
-											  {
-												  UserId = user.UserId,
-												  FirstName = user.Name,
-												  LastName = user.SecondName,
-												  Phone = user.Number,
-												  Pan = card.Pan,
-												  ExpDate = card.ExpDate
-											  };
-		if (!combinedRecords.Any()) return Result<FilePath>.Failure();
+		Result<ICollection<Record>> combinedRecordsResult = await mediator.Send(
+			new GetMatchingRecordsQuery(mathedFilePaths), cancellationToken);
+		if (combinedRecordsResult.IsFailure) return Result<FilePath>.Failure();
 
 		FilePath reportPath = new( Path.Combine(configuration.GetOutputDirectoryExistingPath(),
 			"Report" +	DateTime.Now.ToString("yyyy-dd-MM_HH-mm-ss") + ".json"));
-		RecordsContainer records = new() { Records = combinedRecords.ToList() };
+		RecordsContainer records = new() { Records = combinedRecordsResult.Value!.ToList() };
 		bool success = JsonFileWriter.Write<RecordsContainer>(reportPath, records);
 		return success? Result<FilePath>.Success(reportPath) : Result<FilePath>.Failure();
 	}
---
+++ DataFileCombiner.ClassLibrary/Mediatr/Queries/GetMatchingRecors/GetMatchingRecordsHandler.cs
@@ -0,0 +1,53 @@
+﻿using DataFileCombiner.ClassLibrary.Models;
+using MediatR;
+using System.Collections.Concurrent;
+
+namespace DataFileCombiner.ClassLibrary.Mediatr.Queries;
+public class GetMatchingRecordsHandler(
+	IMediator mediator
+	) : IRequestHandler<GetMatchingRecordsQuery, Result<ICollection<Record>>>
+{
+	public async Task<Result<ICollection<Record>>> Handle(GetMatchingRecordsQuery request, CancellationToken cancellationToken)
+	{
+		// Next is parallel processing with concurrent collections.
+		ConcurrentBag<IEnumerable<User>> usersBag = new();
+		ConcurrentBag<IEnumerable<Card>> cardsBag = new();
+		List<Task> tasks = new List<Task>();
+
+		foreach (FilePath path in request.MatchingFilesPaths.CsvFilePaths)
+		{
+			tasks.Add(Task.Run(async () =>
+			{
+				var result = await mediator.Send(new DeserializeUsersFromCsvQuery(path), cancellationToken);
+				if (result.IsSuccess) usersBag.Add(result.Value!);
+			}));
+		}
+
+		foreach (FilePath path in request.MatchingFilesPaths.XmlFilePaths)
+		{
+			tasks.Add(Task.Run(async () =>
+			{
+				var result = await mediator.Send(new DeserializeCardsFromXmlQuery(path), cancellationToken);
+				if (result.IsSuccess) cardsBag.Add(result.Value!.CardList);
+			}));
+		}
+
+		await Task.WhenAll(tasks);
+		IEnumerable<User> users = usersBag.SelectMany(user => user);
+		IEnumerable<Card> cards = cardsBag.SelectMany(card => card);
+
+		IEnumerable<Record> combinedRecords = from card in cards
+											  join user in users on card.UserId equals user.UserId
+											  select new Record()
+											  {
+												  UserId = user.UserId,
+												  FirstName = user.Name,
+												  LastName = user.SecondName,
+												  Phone = user.Number,
+												  Pan = card.Pan,
+												  ExpDate = card.ExpDate
+											  };
+		if (!combinedRecords.Any()) return Result<ICollection<Record>>.Failure();
+		return Result<ICollection<Record>>.Success(combinedRecords.ToList());
+	}
+}
