d94cffccab93ce08a74bc0f364f3851a2435324b
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-30 10:08:48 +0530

    added deserializers

---
+++ DataFileCombiner.ClassLibrary/Utility/CsvDeserializer.cs
@@ -0,0 +1,32 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using DataFileCombiner.ClassLibrary.Models;
+using System.Globalization;
+
+namespace DataFileCombiner.ClassLibrary.Utility;
+public static class CsvDeserializer
+{
+	public static Result<IEnumerable<T>> ProcessAllRecords<T>(string filePath, string delimiter = ";")
+	{
+		try
+		{
+			var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+			{
+				Delimiter = delimiter,
+				HasHeaderRecord = true,
+			};
+
+			using (var reader = new System.IO.StreamReader(filePath, System.Text.Encoding.UTF8))
+			using (var csv = new CsvReader(reader, configuration))
+			{
+				List<T> records = csv.GetRecords<T>().ToList();
+				if (records.Count == 0) return Result<IEnumerable<T>>.Failure();
+				return Result<IEnumerable<T>>.Success(records);
+			}
+		}
+		catch
+		{
+			return Result<IEnumerable<T>>.Failure();
+		}
+	}
+}
---
+++ DataFileCombiner.ClassLibrary/Utility/XmlDeserializer.cs
@@ -0,0 +1,25 @@
+﻿using DataFileCombiner.ClassLibrary.Models;
+using System.Xml.Serialization;
+
+namespace DataFileCombiner.ClassLibrary.Utility;
+public static class XmlDeserializer
+{
+	public static Result<T> ProcessWholeFile<T>(string filePath)
+	{
+		try
+		{
+			XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+			using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+			{
+				T? newobject = (T?)serializer.Deserialize(fileStream);
+				if (newobject is null) return Result<T>.Failure();
+				return Result<T>.Success(newobject);
+			}
+		}
+		catch 
+		{
+			return Result<T>.Failure();
+		}
+	}
+}
